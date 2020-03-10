057acc88a043185a4c1b11c34f0057e0bd2010fd
Marek Kaput <marek.kaput@swmansion.com>
2020-03-10 10:26:24 +0100

    implement logcontext

--- sebex/log.py
+++ sebex/log.py
@@ -1,11 +1,18 @@
 from contextlib import contextmanager
-from typing import NoReturn
+from contextvars import ContextVar
+from itertools import chain
+from typing import NoReturn, Optional, List
 
 import click
 
+_logcontext_var = ContextVar('sebex_logcontext')
+
 
 def log(*msg, color=None):
-    message = ' '.join(str(m) for m in msg)
+    message = ' '.join(chain(
+        (click.style(f'[{c}]', fg='bright_black') for c in _logcontext_var.get([])),
+        (str(m) for m in msg)
+    ))
     if color is not None:
         message = click.style(message, fg=color)
     click.echo(message)
@@ -34,10 +41,27 @@ def fatal(*msg) -> NoReturn:
 
 @contextmanager
 def operation(*msg):
+    ok_message: str = click.style('OK', fg='green')
+
+    def reporter(message: Optional[str]):
+        nonlocal ok_message
+        if message:
+            ok_message = message
+
     log(*msg, '...')
     try:
-        yield None
-        log(*msg, click.style('OK', fg='green'))
+        yield reporter
+        log(*msg, ok_message)
     except:
         log(*msg, click.style('ERROR', fg='red'))
         raise
+
+
+@contextmanager
+def logcontext(ctx: str):
+    lst: List[str] = _logcontext_var.get([])
+    token = _logcontext_var.set([*lst, ctx])
+    try:
+        yield None
+    finally:
+        _logcontext_var.reset(token)
