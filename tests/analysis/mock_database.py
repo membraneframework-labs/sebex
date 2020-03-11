f27e5fb92491861aa4f2430a210f1abf9dd330a7
Marek Kaput <marek.kaput@swmansion.com>
2020-03-11 13:08:46 +0100

    implement functionality to override specs & locks in tests

--- tests/analysis/mock_database.py
+++ tests/analysis/mock_database.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Dict, Optional
+from typing import Dict, Optional, Callable, Tuple, Union
 
 from sebex.analysis import AnalysisDatabase
 from sebex.analysis import Language, AnalysisEntry, Version, Dependency, VersionSpec
@@ -22,7 +22,36 @@ def _prepare_versions(versions: Optional[Dict[str, str]] = None) -> Dict[str, Ve
                        {p: Version.parse(v) for p, v in versions.items()})
 
 
-def triangle_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
+def _default_spec(_package: str, depends_on: str, versions: Dict[str, Version]) -> VersionSpec:
+    return VersionSpec.targeting(versions[depends_on])
+
+
+VersionSpecProvider = Callable[
+    [str, str, Dict[str, Version]],
+    Union[VersionSpec, Tuple[VersionSpec, Version]]
+]
+
+
+def _dep(name: str, defined_in: str,
+         versions: Dict[str, Version],
+         specs: VersionSpecProvider) -> Dependency:
+    spec_result = specs(defined_in, name, versions)
+    if isinstance(spec_result, tuple):
+        version_spec, version_lock = spec_result
+    else:
+        version_spec, version_lock = spec_result, None
+
+    return Dependency(
+        name=name,
+        defined_in=defined_in,
+        version_spec=version_spec,
+        version_spec_span=Span.ZERO,
+        version_lock=version_lock,
+    )
+
+
+def triangle_db(versions: Dict[str, str] = None,
+                specs: VersionSpecProvider = _default_spec) -> AnalysisDatabase:
     versions = _prepare_versions(versions)
     return MockAnalysisDatabase.mock({
         ProjectHandle.parse('a'): (Language.ELIXIR, AnalysisEntry(
@@ -30,18 +59,8 @@ def triangle_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
             version=versions['a'],
             version_span=Span.ZERO,
             dependencies=[
-                Dependency(
-                    name='b',
-                    defined_in='a',
-                    version_spec=VersionSpec.targeting(versions['b']),
-                    version_spec_span=Span.ZERO
-                ),
-                Dependency(
-                    name='c',
-                    defined_in='a',
-                    version_spec=VersionSpec.targeting(versions['c']),
-                    version_spec_span=Span.ZERO
-                )
+                _dep('b', 'a', versions, specs),
+                _dep('c', 'a', versions, specs)
             ]
         )),
         ProjectHandle.parse('b'): (Language.ELIXIR, AnalysisEntry(
@@ -49,12 +68,7 @@ def triangle_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
             version=versions['b'],
             version_span=Span.ZERO,
             dependencies=[
-                Dependency(
-                    name='c',
-                    defined_in='b',
-                    version_spec=VersionSpec.targeting(versions['c']),
-                    version_spec_span=Span.ZERO
-                )
+                _dep('c', 'b', versions, specs)
             ]
         )),
         ProjectHandle.parse('c'): (Language.ELIXIR, AnalysisEntry(
@@ -65,7 +79,8 @@ def triangle_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
     })
 
 
-def stupid_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
+def stupid_db(versions: Dict[str, str] = None,
+              specs: VersionSpecProvider = _default_spec) -> AnalysisDatabase:
     versions = _prepare_versions(versions)
     return MockAnalysisDatabase.mock({
         ProjectHandle.parse('a'): (Language.ELIXIR, AnalysisEntry(
@@ -78,18 +93,8 @@ def stupid_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
             version=versions['b'],
             version_span=Span.ZERO,
             dependencies=[
-                Dependency(
-                    name='a',
-                    defined_in='b',
-                    version_spec=VersionSpec.targeting(versions['a']),
-                    version_spec_span=Span.ZERO
-                ),
-                Dependency(
-                    name='f',
-                    defined_in='b',
-                    version_spec=VersionSpec.targeting(versions['f']),
-                    version_spec_span=Span.ZERO
-                )
+                _dep('a', 'b', versions, specs),
+                _dep('f', 'b', versions, specs)
             ]
         )),
         ProjectHandle.parse('c'): (Language.ELIXIR, AnalysisEntry(
@@ -97,18 +102,8 @@ def stupid_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
             version=versions['c'],
             version_span=Span.ZERO,
             dependencies=[
-                Dependency(
-                    name='a',
-                    defined_in='c',
-                    version_spec=VersionSpec.targeting(versions['a']),
-                    version_spec_span=Span.ZERO
-                ),
-                Dependency(
-                    name='b',
-                    defined_in='c',
-                    version_spec=VersionSpec.targeting(versions['b']),
-                    version_spec_span=Span.ZERO
-                )
+                _dep('a', 'c', versions, specs),
+                _dep('b', 'c', versions, specs)
             ]
         )),
         ProjectHandle.parse('d'): (Language.ELIXIR, AnalysisEntry(
@@ -116,12 +111,7 @@ def stupid_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
             version=versions['d'],
             version_span=Span.ZERO,
             dependencies=[
-                Dependency(
-                    name='b',
-                    defined_in='d',
-                    version_spec=VersionSpec.targeting(versions['b']),
-                    version_spec_span=Span.ZERO
-                )
+                _dep('b', 'd', versions, specs)
             ]
         )),
         ProjectHandle.parse('e:unused'): (Language.ELIXIR, AnalysisEntry(
@@ -134,12 +124,7 @@ def stupid_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
             version=versions['f'],
             version_span=Span.ZERO,
             dependencies=[
-                Dependency(
-                    name='a',
-                    defined_in='f',
-                    version_spec=VersionSpec.targeting(versions['a']),
-                    version_spec_span=Span.ZERO
-                ),
+                _dep('a', 'f', versions, specs),
             ],
         )),
         ProjectHandle.parse('g'): (Language.ELIXIR, AnalysisEntry(
@@ -147,18 +132,14 @@ def stupid_db(versions: Dict[str, str] = None) -> AnalysisDatabase:
             version=versions['g'],
             version_span=Span.ZERO,
             dependencies=[
-                Dependency(
-                    name='f',
-                    defined_in='g',
-                    version_spec=VersionSpec.targeting(versions['f']),
-                    version_spec_span=Span.ZERO
-                ),
+                _dep('f', 'g', versions, specs),
             ],
         )),
     })
 
 
-def chain_db(height: int = 2, width: int = 1, versions: Dict[str, str] = None) -> AnalysisDatabase:
+def chain_db(height: int = 2, width: int = 1, versions: Dict[str, str] = None,
+             specs: VersionSpecProvider = _default_spec) -> AnalysisDatabase:
     def pkg_name(h: int, w: int) -> str:
         # Hack to always point to a0 package, this removes the need to make conditions on this
         if h == 0:
@@ -184,12 +165,7 @@ def chain_db(height: int = 2, width: int = 1, versions: Dict[str, str] = None) -
                 version=versions[pkg_name(y, x)],
                 version_span=Span.ZERO,
                 dependencies=[
-                    Dependency(
-                        name=pkg_name(y - 1, x),
-                        defined_in=pkg_name(y, x),
-                        version_spec=VersionSpec.targeting(versions[pkg_name(y - 1, x)]),
-                        version_spec_span=Span.ZERO,
-                    )
+                    _dep(pkg_name(y - 1, x), pkg_name(y, x), versions, specs)
                 ]
             ))
 
